a1e10945035ba58fbd1aede277a14771f3eb77c3
Dorttein <sfgas02@gmail.com>
2023-04-07 16:35:25 +0200

    Maquetado listas

---
+++ JS/listas.js
@@ -0,0 +1,29 @@
+$(".show-front").click(function (e) { /* DA LA VUELTA AL PADRE Y AL HERMANO DEL PADRE AL PULSAR EL BOTON */
+    /* DARLES LA VUELTA A LOS DOS DIV */
+    $(e.target).parent("div").siblings("div").css("transform", "perspective( 2000px ) rotateY( 0deg )");
+    $(e.target).parent("div").css("transform", "perspective( 2000px ) rotateY( 180deg )");
+
+    /* MOVERLO AL CENTRO DE LA PANTALLA Y OSCURECER EL RESTO */
+    $(e.target).parent("div").parent("div").css("width", "90vw");
+    $(e.target).parent("div").parent("div").css("position", "absolute");
+    $(e.target).parent("div").parent("div").css("height", "70vh");
+    $(e.target).parent("div").parent("div").css("margin-left", "-50%");
+    $(e.target).parent("div").parent("div").css("z-index", "110");
+    $(".fondoNegro").show();
+});
+
+$(".show-back").click(function (e) { 
+    $(e.target).parent("div").css("transform", "perspective( 2000px ) rotateY( 180deg )");
+    $(e.target).parent("div").siblings("div").css("transform", "perspective( 2000px ) rotateY( 0deg )");   
+
+    $(e.target).parent("div").parent("div").css("position", "static");
+
+    
+    $(e.target).parent("div").parent("div").css("width", "");
+    $(e.target).parent("div").parent("div").css("height", "");
+    $(e.target).parent("div").parent("div").css("margin-left", "");
+    $(e.target).parent("div").parent("div").css("z-index", "");
+    $(".fondoNegro").hide();
+
+});
+
